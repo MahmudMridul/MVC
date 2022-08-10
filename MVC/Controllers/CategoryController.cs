8167f791817b5f8a381ab2f2e58199b6b6947db9
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-08-10 22:40:18 +0600

    Init

---
+++ MVC/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Data;
+using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _database;
+        public CategoryController(ApplicationDbContext database)
+        {
+            _database = database;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _database.Categories;
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //for built in security
+        public IActionResult Create(Category category)
+        {
+            _database.Categories.Add(category);
+            _database.SaveChanges(); 
+            return RedirectToAction("Index");
+        }
+    }
+}
