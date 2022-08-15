2268dcd46fdbe822fa8bf98efa4f0e770a5268c7
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-08-15 20:11:11 +0600

    Sixth

--- MVC/Controllers/ApplicationTypeController.cs
+++ MVC/Controllers/ApplicationTypeController.cs
@@ -16,12 +16,15 @@ namespace MVC.Controllers
         {
             _database = database;
         }
+
+        [HttpGet]
         public IActionResult Index()
         {
             IEnumerable<ApplicationType> applicationTypes = _database.ApplicationTypes;
             return View(applicationTypes);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
@@ -32,6 +35,60 @@ namespace MVC.Controllers
         public IActionResult Create(ApplicationType applicationType)
         {
             _database.ApplicationTypes.Add(applicationType);
+            _database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            ApplicationType applicationType = _database.ApplicationTypes.Find(id);
+
+            if(applicationType == null)
+            {
+                return NotFound();
+            }
+
+            return View(applicationType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType applicationType)
+        {
+            _database.ApplicationTypes.Update(applicationType);
+            _database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            ApplicationType applicationType = _database.ApplicationTypes.Find(id);
+
+            if(applicationType == null)
+            {
+                return NotFound();
+            }
+
+            return View(applicationType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(ApplicationType applicationType)
+        {
+            _database.ApplicationTypes.Remove(applicationType);
             _database.SaveChanges();
             return RedirectToAction("Index");
         }
